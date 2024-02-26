27b245b79ea5502edd8f4a31c2baa86e3ff72c70
Andre-G-C-Araujo <AndreCaue>
2024-02-26 19:57:42 -0300

    working on forms validations and stufs

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-import { ThemeProvider } from 'styled-components'
-import { defaultTheme } from './styles/themes/default'
-import { Button } from './Button'
-import { GlobalStyle } from './styles/global'
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
+import { Router } from "./routes/Router";
+import { BrowserRouter } from "react-router-dom";
 
 export const App = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="danger" />
-      <Button variant="sucess" />
-      <Button />
-
       <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ThemeProvider>
-  )
-}
+  );
+};
